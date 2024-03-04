9e5f22c70290aa6feb64e2eac3f52d5a0c9dc93f
Ananya Shukla <shukla_ananya@outlook.com>
2024-03-04 19:26:34 +0530

    Pushing xception!

--- xception.py
+++ xception.py
@@ -1,13 +1,17 @@
+import os
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
+from PIL import Image
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.models import inception_v3
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
 # Define constants
 SEED = 123
@@ -17,9 +21,34 @@ input_size = 299  # InceptionV3 input size
 batch_size = 32
 
 # Define paths
-train_path = "/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/train_images" # Path to the directory containing training images
+train_path = "/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/Trainset" # Path to the directory containing training images
 test_path =  "/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/test_images" # Path to the directory containing test images
 
+# Load train labels and perform label encoding
+train_df = pd.read_csv('paddy-disease-classification/train.csv')
+
+# Define custom dataset
+class ImageDataset(Dataset):
+    def __init__(self, df, img_dir, transform=None):
+        self.df = df
+        self.img_dir = img_dir
+        self.transform = transform
+        self.classes = df['label'].unique()
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.img_dir, self.df.iloc[idx, 0])
+        image = Image.open(img_name)
+        # label = self.df.iloc[idx, 4]  # Adjusted to use the correct index for 'label_encoded'
+        label = self.df.iloc[idx, self.df.columns.get_loc('label')]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+    
 # Define transforms
 transform = transforms.Compose([
     transforms.Resize((input_size, input_size)),
@@ -27,20 +56,25 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+# Split data into train and validation sets
+train_df, valid_df = train_test_split(train_df, test_size=0.2, random_state=42)
+
 # Load datasets
-train_dataset = ImageFolder(train_path, transform=transform)
-test_dataset = ImageFolder(test_path, transform=transform)
+# Create data loaders
+train_data = ImageDataset(df=train_df, img_dir=train_path, transform=transform)
+valid_data = ImageDataset(df=valid_df, img_dir=train_path, transform=transform)
 
 # Create data loaders
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size)
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(valid_data, batch_size=batch_size)
 
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Create model instance
-num_classes = len(train_dataset.classes)
-model = inception_v3(pretrained=True, aux_logits=False)
+num_classes = len(train_data.classes)
+# model = inception_v3(pretrained=True, aux_logits=False)
+model = inception_v3(pretrained=True, aux_logits=True)
 num_features = model.fc.in_features
 model.fc = nn.Linear(num_features, num_classes)
 model.to(device)
@@ -50,13 +84,19 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 # Training loop
+# Training loop
+# Training loop
+# Training loop
 for epoch in range(EPOCHS):
     model.train()
     for inputs, labels in train_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs = inputs.to(device)
+        labels = torch.tensor([list(train_data.classes).index(label) for label in labels]).to(device)
         optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
+        outputs, aux_outputs = model(inputs)
+        loss1 = criterion(outputs, labels)
+        loss2 = criterion(aux_outputs, labels)
+        loss = loss1 + 0.4*loss2
         loss.backward()
         optimizer.step()
 
@@ -65,7 +105,7 @@ model.eval()
 predictions = []
 true_labels = []
 with torch.no_grad():
-    for inputs, labels in test_loader:
+    for inputs, labels in valid_loader:
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
         _, predicted = torch.max(outputs.data, 1)
