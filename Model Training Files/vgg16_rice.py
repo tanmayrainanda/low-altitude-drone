028b8d0014203fe11ae27961d8adefdbaa5ed70b
Tanmay Rai Nanda <Stormrider7600@gmail.com>
2024-04-02 21:04:38 +0530

    Fixed bugs in VGG16

--- Model Training Files/vgg16_rice.py
+++ Model Training Files/vgg16_rice.py
@@ -1,420 +1,130 @@
 # %%
-train_path = '/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/train_images'
-test_path  = '/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/test_images'
+import os
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import models
 import glob
 from pathlib import Path
+from sklearn.metrics import confusion_matrix
+
+train_path = '/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/train_images'
+test_path = '/Users/ananyashukla/Desktop/low-altitude-drone/paddy-disease-classification/test_images'
 
 for filepath in glob.glob(train_path + '/*/'):
     files = glob.glob(filepath + '*')
     print(f"{len(files)} \t {Path(filepath).name}")
 
 files = glob.glob(test_path + '/*')
-print(f"{len(files)} \t {Path(test_path).name}") 
-
-# %%
-import numpy as np
-import pandas as pd
-import pickle
-import cv2
-import os
-from pathlib import Path
-
-# import tensorflow as tf
-# from tensorflow import keras
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from torchvision import models, transforms
-
-from os import listdir
-from sklearn.preprocessing import LabelBinarizer
-import keras.layers as layers
-from keras.models import Sequential
-# from livelossplot import PlotLossesCallback
-
-from keras.layers import BatchNormalization
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Activation, Flatten, Dropout, Dense
-from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-from keras.utils import plot_model
-
-from keras.preprocessing import image
-from keras.preprocessing.image import img_to_array
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import confusion_matrix
-import keras
-keras.__version__
+print(f"{len(files)} \t {Path(test_path).name}")
 
 SEED = 123
 EPOCHS = 100
-INIT_LR = 1e-3
-BS = 32
-default_image_size = tuple((256, 256))
-image_size = 0
-width = 256
-height = 256
-depth = 3
-
+LR = 1e-3
+BATCH_SIZE = 32
+IMAGE_SIZE = (256, 256)
 n_classes = len(glob.glob(train_path + '/*/'))
-print(n_classes)
+print(f"Number of classes: {n_classes}")
 
-# %%
-import os
-from keras.models import Model
-#from keras.optimizers import Adam
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.layers import Dense, Dropout, Flatten
-from pathlib import Path
-import numpy as np
+class ImageDataset(Dataset):
+    def __init__(self, data_path, transform=None):
+        self.data_path = data_path
+        self.transform = transform
+        self.image_paths = sorted(glob.glob(os.path.join(data_path, '*', '*')))
+        self.class_names = sorted(os.listdir(data_path))
 
-#https://www.learndatasci.com/tutorials/hands-on-transfer-learning-keras/
-def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
-    """
-    Compiles a model integrated with VGG16 pretrained layers
-    
-    input_shape: tuple - the shape of input images (width, height, channels)
-    n_classes: int - number of classes for the output layer
-    optimizer: string - instantiated optimizer to use for training. Defaults to 'RMSProp'
-    fine_tune: int - The number of pre-trained layers to unfreeze.
-                If set to 0, all pretrained layers will freeze during training
-    """
-    
-    # Pretrained convolutional layers are loaded using the Imagenet weights.
-    # Include_top is set to False, in order to exclude the model's fully-connected layers.
-    conv_base = VGG16(include_top=False,
-                     weights='imagenet', 
-                     input_shape=input_shape)
-    
-    # Defines how many layers to freeze during training.
-    # Layers in the convolutional base are switched from trainable to non-trainable
-    # depending on the size of the fine-tuning parameter.
-    if fine_tune > 0:
-        for layer in conv_base.layers[:-fine_tune]:
-            layer.trainable = False
-    else:
-        for layer in conv_base.layers:
-            layer.trainable = False
+    def __len__(self):
+        return len(self.image_paths)
 
-    # Create a new 'top' of the model (i.e. fully-connected layers).
-    # This is 'bootstrapping' a new top_model onto the pretrained layers.
-    top_model = conv_base.output
-    top_model = Flatten(name="flatten")(top_model)
-    top_model = Dense(1024, activation='relu')(top_model)
-    top_model = Dropout(0.2)(top_model)
-    
-    output_layer = Dense(n_classes, activation='softmax')(top_model)
-    
-#     model.add(Dense(1024))
-#     model.add(Activation("relu"))
-#     model.add(BatchNormalization())
-#     model.add(Dropout(0.5))
-#     model.add(Dense(n_classes))
-#     model.add(Activation("softmax"))
-    
-    
-    # Group the convolutional base and new fully-connected layers into a Model object.
-    model = Model(inputs=conv_base.input, outputs=output_layer)
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = Image.open(image_path).convert('RGB')
+        label = self.class_names.index(Path(image_path).parent.name)
 
-    # Compiles the model for training.
-    model.compile(optimizer=optimizer, 
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    
-    return model
+        if self.transform:
+            image = self.transform(image)
 
-# %%
-# Newly Added!!
-net = models.vgg16(pretrained = True)  # Use a pretrained model
+        return image, label
 
-def get_model():
-    model = Sequential()
-    inputShape = (height, width, depth)
-    chanDim = -1
-    print(K.image_data_format())
-    if K.image_data_format() == "channels_first":
-        inputShape = (depth, height, width)
-        chanDim = 1
-    model.add(Conv2D(32, (3, 3), padding="same",input_shape=inputShape))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(3, 3)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(64, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(Conv2D(64, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(128, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(Conv2D(128, (3, 3), padding="same"))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization(axis=chanDim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(1024))
-    model.add(Activation("relu"))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.5))
-    model.add(Dense(n_classes))
-    model.add(Activation("softmax"))
-    
-    # opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-    # optimizer = optim.Adam(net.parameters(), lr=INIT_LR)
-    # opt = tf.keras.optimizers.legacy.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
-    optimizer = Adam(lr=INIT_LR)
+# Define transforms
+transform = transforms.Compose([
+    transforms.Resize(IMAGE_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-    # distribution
-    model.compile(loss="binary_crossentropy", optimizer = optimizer,metrics=["accuracy"])
-    return model
-model = get_model()
+# Load datasets
+train_dataset = ImageDataset(train_path, transform=transform)
+test_dataset = ImageDataset(test_path, transform=transform)
 
-#plot_model(model, '/kaggle/working/model.png', show_shapes=True)
+# Create data loaders
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-# %%
-input_shape = (256, 256, 3)
+# Load pre-trained VGG16 model
+model = models.vgg16(pretrained=True)
 
-# Use a smaller learning rate
-optim_2 = Adam(lr=0.0001)
+# Freeze all layers
+for param in model.parameters():
+    param.requires_grad = False
 
-# Re-compile the model, this time leaving the last 2 layers unfrozen for Fine-Tuning
-vgg16_model = create_model(input_shape, n_classes, optim_2, fine_tune=2)
+# Replace the last fully connected layer
+num_features = model.classifier[6].in_features
+model.classifier[6] = nn.Linear(num_features, n_classes)
 
-# %%
-image_datagen = ImageDataGenerator(featurewise_center=False,
-                 samplewise_center=False,
-                 featurewise_std_normalization=False,
-                 samplewise_std_normalization=False,
-                 zca_whitening=False,
-                 rotation_range=5,
-                 shear_range=0.2,
-                 zoom_range=0.2,
-                 width_shift_range=0.05,
-                 height_shift_range=0.05,
-                 channel_shift_range=0.,
-                 fill_mode='nearest',
-                 horizontal_flip=True,
-                 vertical_flip=False,
-                 rescale=1./255,
-                                   validation_split=0.2)
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
 
-train_generator = image_datagen.flow_from_directory(    
-    directory = train_path,
-    subset='training',
-    target_size=(256, 256),
-    color_mode="rgb",
-    batch_size=32,
-    class_mode="categorical",
-    shuffle=True,
-    seed=SEED)
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=LR)
 
-valid_generator = image_datagen.flow_from_directory(    
-    directory=train_path,
-    subset='validation',
-    target_size=(256, 256),
-    color_mode="rgb",
-    batch_size=32,
-    class_mode="categorical",
-    shuffle=True,
-    seed=SEED)
+# Training loop
+for epoch in range(EPOCHS):
+    model.train()
+    running_loss = 0.0
 
-test_generator = ImageDataGenerator(rescale=1./255).flow_from_directory(    
-    directory=test_path,
-    target_size=(256, 256),
-    color_mode="rgb",
-    batch_size=1,
-    classes=['.'],
-    class_mode=None,
-    shuffle=False,
-    seed=SEED)
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
 
-print(train_generator.class_indices)
-print(test_generator.samples)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
+        running_loss += loss.item()
 
-# ModelCheckpoint callback - save best weights
-tl_checkpoint_1 = ModelCheckpoint(filepath='vgg_model_ft.weights.best.hdf5',
-                                  save_best_only=True,
-                                  verbose=1)
+    epoch_loss = running_loss / len(train_loader)
+    print(f'Epoch [{epoch+1}/{EPOCHS}], Loss: {epoch_loss:.4f}')
 
-# EarlyStopping
-early_stop = EarlyStopping(monitor='val_loss',
-                           patience=10,
-                           restore_best_weights=True,
-                           mode='min')
+# Testing loop
+model.eval()
+predictions = []
+true_labels = []
 
-STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
-STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        predictions.extend(predicted.cpu().numpy())
+        true_labels.extend(labels.cpu().numpy())
 
-# Retrain model with fine-tuning
-vgg16_history = vgg16_model.fit_generator(generator = train_generator,
-                                  steps_per_epoch = STEP_SIZE_TRAIN,
-                                  validation_data = valid_generator,
-                                  validation_steps = STEP_SIZE_VALID,
-                                  callbacks=[tl_checkpoint_1, early_stop],
-                                  verbose=1,epochs=EPOCHS)
+# Calculate accuracy
+accuracy = sum(predictions == true_labels) / len(true_labels)
+print(f'Test Accuracy: {accuracy:.4f}')
 
-# %%
-# !ls -l /kaggle/input
-
-# %%
-test_path_new = '/kaggle/input/pd2-test-images-labels/'
-
-for filepath in glob.glob(test_path_new + '/*/'):
-    files = glob.glob(filepath + '*')
-    print(f"{len(files)} \t {Path(filepath).name}")
-
-# %%
-class_subset = sorted(os.listdir(test_path_new))#[:10] # Using only the first 10 classes
-class_subset
-
-# %%
-
-
-# %%
-test_generator_vgg = ImageDataGenerator(rescale=1./255).flow_from_directory(    
-    directory=test_path_new,
-    target_size=(256, 256),
-    color_mode="rgb",
-    batch_size=1,
-    classes=class_subset,
-    class_mode=None,
-    shuffle=False,
-    seed=SEED)
-
-vgg16_model.load_weights('vgg_model_ft.weights.best.hdf5') # initialize the best trained weights
-# vgg16_model.load_weights('../input/pd2vgg16weightsbest/vgg_model_ft.weights.best.hdf5') # initialize the best trained weights
-
-vgg_preds_ft = vgg16_model.predict(test_generator_vgg)
-vgg_pred_classes_ft = np.argmax(vgg_preds_ft, axis=1)
-
-# %%
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-
-class_names = test_generator_vgg.class_indices.keys()
-
-true_classes = test_generator_vgg.classes
-
-vgg_acc_ft = accuracy_score(true_classes, vgg_pred_classes_ft)
-print("VGG16 Model Accuracy with Fine-Tuning: {:.2f}%".format(vgg_acc_ft * 100))
-
-cls_report = classification_report(true_classes, vgg_pred_classes_ft, 
-                                   target_names=class_names, digits=5)
-print(cls_report)
-
-# Get the names of the ten classes
-class_names = test_generator_vgg.class_indices.keys()
-
-def plot_heatmap(y_true, y_pred, class_names, ax, title):
-    cm = confusion_matrix(y_true, y_pred)
-    sns.heatmap(
-        cm, 
-        annot=True, 
-        square=True, 
-        xticklabels=class_names, 
-        yticklabels=class_names,
-        fmt='d', 
-        cmap=plt.cm.Blues,
-        cbar=False,
-        ax=ax
-    )
-    #ax.set_title(title, fontsize=16)
-    ax.set_xticklabels(ax.get_xticklabels(), fontsize=12, rotation=45, ha="right")
-    ax.set_yticklabels(ax.get_yticklabels(), fontsize=12)
-    ax.set_ylabel('True Label', fontsize=12)
-    ax.set_xlabel('Predicted Label', fontsize=12)
-
-fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-
-plot_heatmap(true_classes, vgg_pred_classes_ft, class_names, ax, title="VGG16")    
-
-#fig.suptitle("Confusion Matrix Model Comparison", fontsize=12)
-#fig.tight_layout()
-#fig.subplots_adjust(top=1.25)
-plt.show()
-cm = confusion_matrix(true_classes, vgg_pred_classes_ft)
+# Confusion Matrix
+cm = confusion_matrix(true_labels, predictions)
+print('Confusion Matrix:')
 print(cm)
 
-# %%
-# %%time
-# STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
-# STEP_SIZE_VALID = valid_generator.n//valid_generator.batch_size
-# history = model.fit_generator(generator = train_generator,
-#                     steps_per_epoch = STEP_SIZE_TRAIN,
-#                     validation_data = valid_generator,
-#                     validation_steps = STEP_SIZE_VALID,
-#                     epochs=EPOCHS)
-
-history = vgg16_history
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(acc) + 1)
-#Train and validation accuracy
-plt.plot(epochs, acc, 'b', label='Training accurarcy')
-plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
-plt.title('Training and Validation accurarcy')
-plt.legend()
-
-plt.figure()
-#Train and validation loss
-plt.plot(epochs, loss, 'b', label='Training loss')
-plt.plot(epochs, val_loss, 'r', label='Validation loss')
-plt.title('Training and Validation loss')
-plt.legend()
-plt.show()
-
-# %%
-STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
-test_generator.reset()
-pred = vgg16_model.predict_generator(test_generator,
-                               steps=STEP_SIZE_TEST,
-                               verbose=1)
-
-# %%
-loss, acc = vgg16_model.evaluate_generator(test_generator, steps=STEP_SIZE_TEST, verbose=1)
-print(acc, loss)
-
-# %%
-pd.Series(train_generator.classes).value_counts()
-
-# %%
-pd.Series(test_generator.classes).value_counts()
-
-# %%
-predicted_class_indices=np.argmax(pred,axis=1)
-labels = (train_generator.class_indices)
-
-labels = (train_generator.class_indices)
-labels = dict((v,k) for k,v in labels.items())
-predictions = [labels[k] for k in predicted_class_indices]
-
-pd.Series(predictions).value_counts()
-
-# %%
-filenames=test_generator.filenames
-
-results=pd.DataFrame({"image_id":filenames,
-                      "label":predictions})
-results.image_id = results.image_id.str.replace('./', '')
-results.to_csv("submission.csv",index=False)
-results.head()
-
-
+# Save the model
+torch.save(model.state_dict(), 'vgg16_model.pth')
